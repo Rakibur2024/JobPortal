c275d41d7046e7102600d61c14eca973ec79af5c
Rakibur2024 <rrahmanemail@gmail.com>
2025-10-26 21:22:08 +0600

    Started Project

---
+++ src/test/java/com/embarkxx/microservice/JobAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarkxx.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
