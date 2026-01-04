7e03e1ad68e79dbc2c21d76368741286e66bf49c
Rakibur2024 <rrahmanemail@gmail.com>
2026-01-05 00:22:44 +0600

    Completed Review

--- src/main/java/com/embarkxx/microservice/Review/ReviewController.java
+++ src/main/java/com/embarkxx/microservice/Review/ReviewController.java
@@ -1,14 +1,62 @@
 package com.embarkxx.microservice.Review;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/companies/{companyId}}")
+@RequestMapping("/companies/{companyId}")
 public class ReviewController {
     private ReviewService reviewService;
 
     public ReviewController(ReviewService reviewService) {
         this.reviewService = reviewService;
+    }
+
+    @GetMapping("/reviews")
+    public ResponseEntity<List<Review>> getAllReviews(@PathVariable Long companyId){
+        List<Review> reviews = reviewService.getAllReviews(companyId);
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
+
+    @PostMapping("/reviews")
+    public ResponseEntity<String> addReview(@RequestBody Review review, @PathVariable Long companyId){
+        boolean isReviewSaved = reviewService.addReview(companyId,review);
+        if (isReviewSaved){
+            return new ResponseEntity<>("Review Added Successfully!", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Review not saved!", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/review/{reviewId}")
+    public ResponseEntity<Review> getReview(@PathVariable("companyId") Long companyId,
+                                            @PathVariable("reviewId") Long reviewId){
+        return new ResponseEntity<>(reviewService.getReview(companyId,reviewId),HttpStatus.OK);
+    }
+
+    @PutMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> updateReview(@PathVariable("companyId") Long companyId,
+                                               @PathVariable("reviewId") Long reviewId,
+                                               @RequestBody Review review){
+        boolean isReviewUpdated = reviewService.updateReview(companyId,reviewId,review);
+        if(isReviewUpdated){
+            return new ResponseEntity<>("Review updated", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Review not updated", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/reviews/{reviewId}")
+    public ResponseEntity<String> deleteReview(@PathVariable("companyId") Long companyId,
+                                               @PathVariable("reviewId") Long reviewId){
+        boolean isReviewDeleted = reviewService.deleteReview(companyId,reviewId);
+        if(isReviewDeleted){
+            return new ResponseEntity<>("Review deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Review not deleted", HttpStatus.NOT_FOUND);
+        }
     }
 }
